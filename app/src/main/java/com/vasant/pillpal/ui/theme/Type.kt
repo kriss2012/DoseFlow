e9552c6b5c9a297b64441272c494341a646832f5
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-12 19:10:20 +0530

    Update Type.kt

--- app/src/main/java/com/vasant/pillpal/ui/theme/Type.kt
+++ app/src/main/java/com/vasant/pillpal/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.vasant.pillpal.ui.theme
+package com.PillPal.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
@@ -6,7 +6,7 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.vasant.pillpal.R
+import com.PillPal.R
 
 val jetbrainFamily = FontFamily(
     Font(R.font.regular, FontWeight.Normal),
