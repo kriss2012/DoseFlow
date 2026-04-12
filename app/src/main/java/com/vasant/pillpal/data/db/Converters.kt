ffc5cc1f7ce067ec0cd9ebd49120b451a2cb312d
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-12 19:07:45 +0530

    Update Converters.kt

--- app/src/main/java/com/vasant/pillpal/data/db/Converters.kt
+++ app/src/main/java/com/vasant/pillpal/data/db/Converters.kt
@@ -1,7 +1,7 @@
-package com.vasant.pillpal.data.db
+package com.PillPal.data.db
 
 import androidx.room.TypeConverter
-import com.vasant.pillpal.ui.presentation.MedicineType
+import com.PillPal.ui.presentation.MedicineType
 
 class MedicineTypeConverter {
     @TypeConverter
