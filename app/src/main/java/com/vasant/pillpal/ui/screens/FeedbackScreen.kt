7294bca9a3bd53edd5cec660cd6dbe5467e57a4a
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-12 19:09:39 +0530

    Update FeedbackScreen.kt

--- app/src/main/java/com/vasant/pillpal/ui/screens/FeedbackScreen.kt
+++ app/src/main/java/com/vasant/pillpal/ui/screens/FeedbackScreen.kt
@@ -1,4 +1,4 @@
-package com.vasant.pillpal.ui.screens
+package com.PillPal.ui.screens
 
 import android.content.Intent
 import android.net.Uri
@@ -24,10 +24,10 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.vasant.pillpal.ui.theme.BackgroundColor
-import com.vasant.pillpal.ui.theme.SecondaryContainerColor
-import com.vasant.pillpal.ui.theme.fontColor
-import com.vasant.pillpal.ui.theme.jetbrainFamily
+import com.PillPal.ui.theme.BackgroundColor
+import com.PillPal.ui.theme.SecondaryContainerColor
+import com.PillPal.ui.theme.fontColor
+import com.PillPal.ui.theme.jetbrainFamily
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
