310a798fdfcab62323d1cad97dc617b51b58bb19
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-12 19:10:28 +0530

    Update FirebaseViewModel.kt

--- app/src/main/java/com/vasant/pillpal/ui/viewmodel/FirebaseViewModel.kt
+++ app/src/main/java/com/vasant/pillpal/ui/viewmodel/FirebaseViewModel.kt
@@ -1,16 +1,13 @@
-package com.vasant.pillpal.ui.viewmodel
+package com.PillPal.ui.viewmodel
 
-import android.content.Context
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.vasant.pillpal.repository.Auth
-import com.vasant.pillpal.ui.screens.AddMedsScreenPill
-import com.vasant.pillpal.utils.AuthState
-import com.vasant.pillpal.utils.FirebaseState
+import com.PillPal.repository.Auth
+import com.PillPal.utils.AuthState
+import com.PillPal.utils.FirebaseState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -19,9 +16,9 @@ import javax.inject.Inject
 class FirebaseViewModel @Inject constructor(
     private val auth: Auth
 ) : ViewModel() {
-    var firebaseState by mutableStateOf<FirebaseState?>(value = null)
+    var firebaseState by mutableStateOf<FirebaseState?>(null)
     //    var authState by mutableStateOf<AuthResult>(AuthResult.Loading)
-    var authState by mutableStateOf<AuthState>(AuthState(Error = null, Success = false))
+    var authState by mutableStateOf(AuthState(Error = null, Success = false))
 
     fun login(email: String, password: String) {
         viewModelScope.launch {
