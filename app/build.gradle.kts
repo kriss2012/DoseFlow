647b06896283f9bd58c15c94b5839e241ee5dc39
Krishna Patil <krishnacpatil2006@gmail.com>
2026-04-13 09:11:03 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.PillPal"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.2"
+        versionCode = 11
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
